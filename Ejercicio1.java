739c149c010a5eb0e3fdd2e2ddb19af79d2283bc
Ulises <ulimarian2000@gmail.com>
2021-10-02 18:40:44 -0300

    ejercicio 1

---
+++ Ejercicio1.java
@@ -0,0 +1,13 @@
+package com.informatorio;
+
+import java.util.Scanner;
+
+public class Ejercicio1{
+
+    public static void main(String[] args){
+        Scanner solicitarDatos = new Scanner(System.in);
+
+        System.out.println("Hola" + solicitarDatos.nextLine());
+
+    }
+}

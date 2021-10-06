e2b37afb78bdb2cd617552b16b856f1e77c18084
Ulises Mariano Melgarejo <73831325+Nemo3003@users.noreply.github.com>
2021-10-06 20:01:14 +0000

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -6,7 +6,10 @@ public class Ejercicio_1 {
     public static void main(String[] args){
 
         Scanner introducDatos = new Scanner(System.in);
-        System.out.println("Hola" + introducDatos);
+        System.out.println("Hola, por favor introduce tu nombre: ");
+        String nombre = introducDatos.nextLine();
+        System.out.println("¡Hola " + nombre+ " !");
+
     }
 
 }
